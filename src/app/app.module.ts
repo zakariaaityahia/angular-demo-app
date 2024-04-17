7418a9ff5e1413393d2ecdd84961965f84d7eadf
zakariaeightyahia <zakariaeightyahia@gmail.com>
2024-04-17 20:03:37 +0100

    first demo, 2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,10 @@ import { ProductsComponent } from './products/products.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+     
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+
+  public products : any;
+  public keyword: String="";
+  
+  constructor() {
+
+    }
+
+    ngOnInit(): void {
+        this.products =  [
+          {"id": 1, "name": "smartphone", "price" :3000},
+          {"id": 2, "name": "LAPTOP", "price" :10000},
+          {"id": 3, "name": "Mouse", "price" :250},
+          {"id": 4, "name": "Monitor", "price" :3500}
+      
+        ] ; 
+    }
+
+    deleteProduct(p : any) {
+      let index = this.products.indexOf(p);
+      this.products.splice(index, 1);
+    }
+
+    search() {
+      // console.log(this.keyword)
+      // let result = [];
+      // for(let p of this.products) {
+      //   if(p.name.includes(this.keyword)){
+      //     result.push(p)
+      //   }
+      // }
+      // this.products = result;
+
+      this.products = this.products.filter((p:any)=>p.name.includes(this.keyword))
+
+    }
+
 
 }
